a769c65aecbec8abe6f9259dee5788fb20211948
Jeff DiNoto <jeff.dinoto@gmail.com>
2018-04-10 14:23:08 -0400

    Make the app actually run
    It does nothing.

---
+++ src/main/java/com/riverbed/itim/thirdpartylibs/Main.java
@@ -0,0 +1,14 @@
+package com.riverbed.itim.thirdpartylibs;
+/**
+ * Riverbed Technology
+ * Copyright 2017
+ * All Rights Reserved.
+ *
+ * @author jeffry.dinoto@riverbed.com
+ * @created 4/10/18
+ */
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello, I'm Main, I don't really do anything.");
+    }
+}
